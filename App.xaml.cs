97e61fa6d4ee2df8089008f3d9f6ba1666f47862
Nicholas Tay <nexerq@gmail.com>
2018-11-21 00:27:38 +1100

    Single instance-ify

--- App.xaml.cs
+++ App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -16,6 +17,27 @@ namespace Verdant
         public App() : base()
         {
             this.Dispatcher.UnhandledException += OnDispatcherUnhandledException;
+        }
+
+        // single instance ~ some from https://stackoverflow.com/questions/19147/what-is-the-correct-way-to-create-a-single-instance-wpf-application
+        static Mutex siMutex = new Mutex(true, "NexerqDev.Verdant-SingleInstance");
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            if (!siMutex.WaitOne(TimeSpan.Zero, true))
+            {
+                MessageBox.Show("Only one instance of Verdant may be running at the same time!", "Error - Verdant", MessageBoxButton.OK, MessageBoxImage.Error);
+                Application.Current.Shutdown();
+                siMutex = null;
+                return;
+            }
+            base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            if (siMutex != null)
+                siMutex.ReleaseMutex();
+            base.OnExit(e);
         }
 
         void OnDispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
